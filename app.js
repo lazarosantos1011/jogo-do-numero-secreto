becbb13b73918b914c77763e42f144942bf442ad
Lázaro Rocha <lazaro.rocha1011@gmail.com>
2024-02-21 18:40:19 -0300

    Alterando o número limite de 10 para 20

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 20;
 let numeroSecreto = gerarNumeroAleatorio();
 let numeroTentativas = 1;
 
@@ -17,7 +17,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial(){
 exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 20');
 }
 
 exibirMensagemInicial();
